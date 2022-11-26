1c435883abecf89caf40fc4f16e58b3f780a379e
Bernadette Christiner <b.christiner93@gmail.com>
2022-11-26 18:41:27 +0100

    Remove comments

--- client/src/Pages/page-home.js
+++ client/src/Pages/page-home.js
@@ -28,7 +28,6 @@ const sendInput = (search, setSpecies) => {
 const PageHome = () => {
   const [species, setSpecies] = useState();
   const [search, setSearch] = useState("");
-  // const [details, setDetails] = useState({});
 
   useEffect(() => {
     fetchSpecies(setSpecies);
@@ -66,69 +65,3 @@ const PageHome = () => {
 };
 
 export default PageHome;
-
-// import React, { useEffect, useState } from "react";
-// import ComListSpecies from "../Components/com-list-species";
-
-// const fetchSpecies = (dataSetter) => {
-//   return fetch("http://localhost:8080/api/species/").then((res) =>
-//     res
-//       .json()
-//       .then((data) => this.parseData(data))
-//       .then((data) => {
-//         dataSetter(data);
-//       })
-//   );
-// };
-
-// const sendInput = (search, setSpecies) => {
-//   return fetch("http://localhost:8080/api/species/search", {
-//     method: "POST",
-//     headers: {
-//       "Content-type": "application/json",
-//     },
-//     body: JSON.stringify({
-//       search,
-//     }),
-//   })
-//     .then((result) => {
-//       return result.json();
-//     })
-//     .then((data) => {
-//       setSpecies(data);
-//     });
-// };
-
-// const PageHome = () => {
-//   const [species, setSpecies] = useState();
-//   const [search, setSearch] = useState("");
-//   // const [details, setDetails] = useState({});
-
-//   useEffect(() => {
-//     fetchSpecies(setSpecies);
-//   }, []);
-
-//   useEffect(() => {
-//     const timer = setTimeout(() => {
-//       sendInput(search, setSpecies);
-//     }, 500);
-
-//     return () => clearTimeout(timer);
-//   }, [search]);
-
-//   const searchSpecies = (e) => {
-//     setSearch(e.target.value);
-//   };
-
-//   console.log(species);
-
-//   return (
-//     <div>
-//       <h1>Welcome to Tree Of Life Project</h1>
-//       <input type="text" value={search} onChange={searchSpecies}></input>
-//       <ComListSpecies species={species}></ComListSpecies>
-//     </div>
-//   );
-// };
-
-// export default PageHome;
